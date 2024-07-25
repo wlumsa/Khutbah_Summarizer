38b5ff5c88c500fd072d76ff3de76c7f28485e96
Usama Mohiuddin <usamamohiudin@gmail.com>
2024-07-25 12:55:51 -0400

    removed Temp Runner File

--- src.py
+++ src.py
@@ -1,3 +1,5 @@
+# https://pypi.org/project/youtube-transcript-api/
+
 from youtube_transcript_api import YouTubeTranscriptApi
 from youtube_transcript_api.formatters import TextFormatter
 
@@ -5,17 +7,19 @@ from youtube_transcript_api.formatters import TextFormatter
 video_link = 'https://www.youtube.com/watch?v=liJVSwOiiwg&ab_channel=WebbyFan'
 
 stringToList = list(video_link)
-s = stringToList[32:43]
-listToStr = ''.join([str(elem) for elem in s])
-print(f"VideoID = {listToStr}")
+s = stringToList[32:43] #grabs id
+video_id = ''.join([str(elem) for elem in s])  #converts id to string
+print(f"VideoID = {video_id}")
 
-
-video_id = listToStr
 transcript = YouTubeTranscriptApi.get_transcript(video_id, languages=['en'])
 
 formatter = TextFormatter()
 text_formatted = formatter.format_transcript(transcript)
 print(text_formatted)
+
+
+
+#Now that we got the text we need to summarize it 
 
 
 
